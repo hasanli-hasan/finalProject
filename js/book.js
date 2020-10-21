672c86909271f8c241e408d676b06a2752e9ca6b
hasanli-hasan <hasanih@code.edu.az>
2020-10-21 22:14:30 +0400

    End Book Page

--- js/book.js
+++ js/book.js
@@ -1,4 +1,33 @@
 $('.carousel').carousel({
     interval: 3000
     
-  })
+  });
+
+
+  //timely modal info start
+
+  $(document).ready(function(){
+
+    function ShowWindow(){
+       $('#main').show();
+       //$('html body').css('overflow','hidden');
+
+       setTimeout(hideWindow,20000)
+    }
+    //ShowWindow()
+
+    function hideWindow(){
+      $('#main').hide();
+      //$('html body').css('overflow','scroll');
+    }
+
+   // hideWindow()
+
+   setTimeout(ShowWindow,10000)
+   
+   $('#close-btn-btn').click(function(){
+    hideWindow()
+   })
+  });
+
+  //timely info modal end
